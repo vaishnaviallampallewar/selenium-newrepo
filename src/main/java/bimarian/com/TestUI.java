667914f15e2c3b08cd5cf68c57ac094acfe173f6
vishakhathakur <vishakha_thakur@persistent.co.in>
2017-06-08 17:01:04 +0530

    Create TestUI.java

--- src/main/java/bimarian/com/TestUI.java
+++ src/main/java/bimarian/com/TestUI.java
@@ -1,26 +1,59 @@
-package bimarian.com;
+package com.stm.test;
+
+import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.support.ui.Select;
+
 import org.testng.annotations.Test;
 
-public class TestUI {
-	
-	//@Test
-	public void test01Firefox(){
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://google.com");
-		System.out.println("Firefox browser opened and navigated to google site");
-		driver.quit();
-	}
-	
-	@Test
-	public void test02Chrome(){
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://google.com");
-		System.out.println("Chrome browser opened and navigated to google site");
-		driver.quit();
-	}
+public class TestUI
+
+{
+
+@Test //This is TestNG annotation
+
+public void testRegister()
+
+{
+
+WebDriver driver = new FirefoxDriver();
+
+driver.get(“http://newtours.demoaut.com/”);
+
+driver.findElement(By.linkText(“REGISTER”)).click();
+
+driver.findElement(By.name(“firstName”)).sendKeys(“User1”);
+
+driver.findElement(By.name(“lastName”)).sendKeys(“Surname1”);
+
+driver.findElement(By.name(“phone”)).sendKeys(“123456789”);
+
+driver.findElement(By.name(“userName”)).sendKeys(“user1@test.com”);
+
+driver.findElement(By.name(“address1”)).sendKeys(“Test Address”);
+
+driver.findElement(By.name(“city”)).sendKeys(“Test City”);
+
+Select select = new Select(driver.findElement(By.name(“country”)));
+
+       select.selectByVisibleText(“ANGOLA”);
+
+driver.findElement(By.name(“email”)).sendKeys(“user1@test.com”);
+
+driver.findElement(By.name(“password”)).sendKeys(“user1”);
+
+driver.findElement(By.name(“confirmPassword”)).sendKeys(“user1”);
+
+driver.findElement(By.name(“register”)).click();
+
+driver.close();
+
+driver.quit();
+
+}
+
 }
