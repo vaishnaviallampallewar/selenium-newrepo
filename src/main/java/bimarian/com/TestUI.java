c516107a5f2cd334898bba91dff72d9902d9823c
vishakhathakur <vishakha_thakur@persistent.co.in>
2017-06-14 13:01:32 +0530

    Create TestUI.java

--- src/main/java/bimarian/com/TestUI.java
+++ src/main/java/bimarian/com/TestUI.java
@@ -10,7 +10,7 @@ public class TestUI {
 	//@Test
 	public void test01Firefox(){
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://bimarian.com");
+		driver.get("http://blazemeter.com");
 		System.out.println("Firefox browser opened and navigated to google site");
 		driver.quit();
 	}
@@ -20,8 +20,12 @@ public class TestUI {
 		
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://bimarian.com");
+		driver.get("http://blazemeter.com");
 		System.out.println("Chrome browser opened and navigated to google site");
+		WebElement title = driver.findElement(By.xpath("//title"));
+   	 	System.out.println("********************************************************");
+   	        System.out.println("*" + title.getAttribute("text") + "*");
+     	        System.out.println("********************************************************")
 		driver.quit();
 	}
 }
